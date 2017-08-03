a6d7febd719a7257e533d35db92fa28aaaf88601
Callum John <b6010572@my.shu.ac.uk>
2017-08-03 11:46:18 +0100

    Encountered a bug where the cursor does not change correctly when looking at objects.

--- src/Soluna's Secret/Assets/_Own Assets/Scripts/UI/Crosshair.cs
+++ src/Soluna's Secret/Assets/_Own Assets/Scripts/UI/Crosshair.cs
@@ -4,6 +4,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+// Requirement(s)
+[RequireComponent(typeof(Image))]
+
 public class Crosshair : MonoBehaviour
 {
     enum CrosshairMode
@@ -17,6 +20,8 @@ public class Crosshair : MonoBehaviour
     private Transform firstPersonCharacter;
     [SerializeField]
     private Sprite defaultSprite, lookSprite, useSprite;
+    [SerializeField]
+    private float reach = 4.0f;
 
     private CrosshairMode currentCrosshairMode;
     private Image crosshairIcon;
@@ -24,10 +29,12 @@ public class Crosshair : MonoBehaviour
 
     void Start ()
 	{
+        // Get references to the necessary components
         rectTransform = GetComponent<RectTransform>();
         crosshairIcon = GetComponent<Image>();
 
-        currentCrosshairMode = CrosshairMode.Use;
+        // Reset the crosshair mode to the default
+        currentCrosshairMode = CrosshairMode.Default;
 	} // End void Start ()
 
 	void Update ()
@@ -51,10 +58,29 @@ public class Crosshair : MonoBehaviour
 
     void FixedUpdate()
     {
-        
+        // Set up temporary variables for raycasting
+        Vector3 forwardLookVector = firstPersonCharacter.TransformDirection(Vector3.forward);
+        RaycastHit crosshairHit;
+        Debug.DrawRay(firstPersonCharacter.position, forwardLookVector, Color.red);
+
+        // Look for objects ahead of you
+        if (Physics.Raycast(firstPersonCharacter.position, forwardLookVector, out crosshairHit, reach) &&
+            crosshairHit.transform.tag == "Lookable")
+        {
+            currentCrosshairMode = CrosshairMode.Look;
+        } // End if (Physics.Raycast(firstPersonCharacter.position, forwardLookVector, out crosshairHit, reach) && ...
+        else if (Physics.Raycast(firstPersonCharacter.position, forwardLookVector, out crosshairHit, reach) &&
+                 crosshairHit.transform.tag == "Usable")
+        {
+            currentCrosshairMode = CrosshairMode.Use;
+        } // End else if (Physics.raycast)
+        else
+        {
+            currentCrosshairMode = CrosshairMode.Default;
+        } // End else (Physics.raycast)
     } // End void Fixed
 
-    void FixSizeDeltaToSpriteRect(Sprite sprite)
+    private void FixSizeDeltaToSpriteRect(Sprite sprite)
     {
         crosshairIcon.sprite = sprite;
         rectTransform.sizeDelta = new Vector2(sprite.rect.width, sprite.rect.height);
