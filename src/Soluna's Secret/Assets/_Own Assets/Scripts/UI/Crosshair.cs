404a0c24723737b90042d334aa97b05dd308a273
Callum John <b6010572@my.shu.ac.uk>
2017-08-03 10:53:37 +0100

    Laid the groundwork for the onscreen cursor and created associated graphics for it.

---
+++ src/Soluna's Secret/Assets/_Own Assets/Scripts/UI/Crosshair.cs
@@ -0,0 +1,62 @@
+﻿// Libraries
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Crosshair : MonoBehaviour
+{
+    enum CrosshairMode
+    {
+        Default,
+        Look,
+        Use
+    }; // End enum CrosshairMode
+
+    [SerializeField]
+    private Transform firstPersonCharacter;
+    [SerializeField]
+    private Sprite defaultSprite, lookSprite, useSprite;
+
+    private CrosshairMode currentCrosshairMode;
+    private Image crosshairIcon;
+    private RectTransform rectTransform;
+
+    void Start ()
+	{
+        rectTransform = GetComponent<RectTransform>();
+        crosshairIcon = GetComponent<Image>();
+
+        currentCrosshairMode = CrosshairMode.Use;
+	} // End void Start ()
+
+	void Update ()
+	{
+        switch (currentCrosshairMode)
+        {
+            case CrosshairMode.Default:
+                FixSizeDeltaToSpriteRect(defaultSprite);
+                break;
+            case CrosshairMode.Look:
+                FixSizeDeltaToSpriteRect(lookSprite);
+                break;
+            case CrosshairMode.Use:
+                FixSizeDeltaToSpriteRect(useSprite);
+                break;
+            default:
+                Debug.LogError(name + "'s currentCrosshairMode switch statement broke!");
+                break;
+        } // End switch(currentCrosshairMode)
+    } // End void Update ()
+
+    void FixedUpdate()
+    {
+        
+    } // End void Fixed
+
+    void FixSizeDeltaToSpriteRect(Sprite sprite)
+    {
+        crosshairIcon.sprite = sprite;
+        rectTransform.sizeDelta = new Vector2(sprite.rect.width, sprite.rect.height);
+    } // End void FixSizeDeltaToSpriteRect(Sprite sprite)
+} // End public class Crosshair
