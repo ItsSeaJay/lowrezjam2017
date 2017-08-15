2b530c8cf4fb6cec43384d999d2fe7c0d0f05f03
Callum John <b6010572@my.shu.ac.uk>
2017-08-15 18:35:34 +0100

    Started attempting to warp the player to the credits.

---
+++ src/Soluna's Secret/Assets/LoadSceneOnTriggerEnter.cs
@@ -0,0 +1,20 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneOnTriggerEnter : MonoBehaviour
+{
+    [SerializeField]
+    private string sceneToLoad;
+
+    void OnCollisionEnter(Collision col)
+    {
+        Debug.Log("Helloo");
+        if (col.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene(sceneToLoad);
+        }
+    }
+} // End public class LoadSceneOnTriggerEnter
