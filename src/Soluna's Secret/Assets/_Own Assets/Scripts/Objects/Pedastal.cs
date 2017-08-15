22edf0e5670cb08479281474e668c71eb04e78e5
Callum John <b6010572@my.shu.ac.uk>
2017-08-15 15:56:26 +0100

    Started adding pedastals for the major puzzle to the game.

---
+++ src/Soluna's Secret/Assets/_Own Assets/Scripts/Objects/Pedastal.cs
@@ -0,0 +1,17 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pedastal : MonoBehaviour
+{
+	void Start ()
+	{
+		
+	} // End void Start ()
+
+	void Update ()
+	{
+		
+	} // End void Update ()
+} // End public class Pedastal
